91988340ab96f95bfe23f97aa5678d56493f8772
qiuzhihai <2374008607@qq.com>
2026-03-10 18:31:32 +0800

    2

--- apps/demo/src/App.tsx
+++ apps/demo/src/App.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import GlobalEconomicDashboard from "./pages/GlobalEconomicDashboard";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/global-economy" element={<GlobalEconomicDashboard />} />
     </Routes>
   );
 }
